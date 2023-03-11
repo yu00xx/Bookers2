06b555ae6f89332ae7fabd6d65323ac9748094b8
EC2 Default User <ec2-user@ip-172-31-46-239.ap-northeast-1.compute.internal>
2023-03-11 05:29:27 +0000

    [add]アクセス制限

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,8 +6,14 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+      flash[:notice]="You have created book successfully."
+      redirect_to book_path(@book.id)
+    else
+      @user = current_user
+      @books = Book.all
+      render :index
+    end
   end
 
   def index
@@ -17,13 +23,30 @@ class BooksController < ApplicationController
   end
 
   def show
-    @user = current_user
     @book = Book.find(params[:id])
+    @newbook = Book.new
+    @user = @book.user
   end
 
   def edit
     @book = Book.find(params[:id])
+    if @book.user == current_user
+      render :edit
+    else
+      redirect_to books_path
+    end
   end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice]="You have updated book successfully."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
+
 
   def destroy
     book = Book.find(params[:id])
